461a6380d2dabf2686b66117cd5771719d5445df
hui.zhang <hui.zhang@bitdeer.com>
2024-06-17 18:00:22 +0800

    feat: update

--- mian.go
+++ mian.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"io"
+)
+
 func main()  {
-	
+	url := "https://www.thepaper.cn/"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("http get err:%v", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("http error code: %v", resp.StatusCode)
+		return
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read io err:%v", err)
+		return
+	}
+	fmt.Println("body:" + string(body))
 }
