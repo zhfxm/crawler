c1edf2be1935ab1dfc5f64e95831566e79d3b785
hui.zhang <hui.zhang@bitdeer.com>
2024-07-08 11:44:35 +0800

    feat: fan out

--- mian.go
+++ mian.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	demo1()
-	demo2()
+	// demo1()
+	// fanIn()
+	fanOut1()
+	// time.Sleep(10 * time.Second)
 }
 
 func demo1() {
@@ -28,14 +31,16 @@ func player(table chan int) {
 	table <- ball
 }
 
-func demo2() {
+func fanIn() {
 	ch1 := search("jonson")
 	ch2 := search("alan")
-	select{
-	case msg := <-ch1: 
-	fmt.Println(msg)
-	case msg := <-ch2:
-		fmt.Println(msg)
+	for {
+		select{
+			case msg := <-ch1: 
+			fmt.Println(msg)
+			case msg := <-ch2:
+				fmt.Println(msg)
+			}
 	}
 }
 
@@ -50,4 +55,35 @@ func search(msg string) chan string {
 	}
 	}()
 	return ch
+}
+
+func fanOut1() {
+	var wg sync.WaitGroup
+	wg.Add(36)
+	go pool1(&wg, 36, 50)
+	wg.Wait()
+}
+
+func pool1(wg *sync.WaitGroup, workers, tasks int) {
+	taskCh := make(chan int)
+	for i := 0; i < workers; i++ {
+		go worker1(taskCh, wg)
+	}
+	for i := 0; i < tasks; i++ {
+		taskCh <- i
+	}
+	close(taskCh)
+}
+
+func worker1(taskCh <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		task, ok := <- taskCh
+		if !ok {
+			return
+		}
+		// d := time.Duration(task) * time.Millisecond
+		// time.Sleep(d)
+		fmt.Println("processing task", task)
+	}
 }
