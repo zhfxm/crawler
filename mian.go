459e6290e346db84e27be32852a1c453d5f2c595
hui.zhang <hui.zhang@bitdeer.com>
2024-07-08 15:56:50 +0800

    feat: pipeline

--- mian.go
+++ mian.go
@@ -9,8 +9,9 @@ import (
 func main() {
 	// demo1()
 	// fanIn()
-	fanOut1()
+	// fanOut1()
 	// time.Sleep(10 * time.Second)
+	findNum()
 }
 
 func demo1() {
@@ -85,5 +86,32 @@ func worker1(taskCh <-chan int, wg *sync.WaitGroup) {
 		// d := time.Duration(task) * time.Millisecond
 		// time.Sleep(d)
 		fmt.Println("processing task", task)
+	}
+}
+
+func findNum() {
+	ch := make(chan int)
+	go generator(ch)
+	for i := 0; i < 10; i++ {
+		prime := <-ch
+		fmt.Println(prime)
+		out := make(chan int)
+		go filter(ch, out, prime)
+		ch = out
+	}
+}
+
+func generator(ch chan int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
+}
+
+func filter(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in
+		if i % prime != 0 {
+			out <- i
+		}
 	}
 }
